4aefd61bef654c6e7dce8cabe3b4c6a3b96a42ef
Zibbly <owenhedwards@gmail.com>
2022-07-27 22:56:39 +0100

    Add loading spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Form}  from './components'
+import ClipLoader from "react-spinners/ClipLoader";
 
-function App() {
+const App: React.FC = () => {
+  const [pokemon, setPokemon] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+
+  const renderLoading = () => {
+    if(isLoading){
+      return (
+        <ClipLoader size={150} />
+      )
+    }
+    return (
+      <>
+        <Form setPokemon={setPokemon} setIsLoading={setIsLoading}/>
+
+        {(pokemon.length > 0)
+          ? pokemon
+          : 'goodbye' 
+        }
+      </>
+    )
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {renderLoading()}
     </div>
   );
 }
+
 
 export default App;
